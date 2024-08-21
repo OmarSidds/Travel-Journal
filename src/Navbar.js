c845f655ba0c00939b35c3e1cc9657d9d8d08085
OmarSidds <101479068+OmarSidds@users.noreply.github.com>
2024-08-21 15:33:48 -0400

    navbar completed, added all data for main content section

---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Navbar.css'
+
+export default function Navbar() {
+    return (
+        <nav className='navbar'>
+            <span class="material-symbols-outlined">globe</span>
+            <h1>My Travel Journal</h1>
+        </nav>
+    )
+}
---
+++ src/data.js
@@ -0,0 +1,29 @@
+export default [
+    {
+        id:1,
+        img:"https://assets.simpleviewinc.com/simpleview/image/upload/c_fill,f_jpg,h_640,q_65,w_640/v1/clients/toronto/cn_tower_toronto_276bdc20-b872-4384-ad01-f68bb28d6c2d.jpg",
+        name:"Toronto",
+        googleMaps:"https://www.google.com/maps/place/CN+Tower/@43.6425662,-79.3896317,17z/data=!3m2!4b1!5s0x882b34d819a55ff7:0xad7cf7bcaf4e239b!4m6!3m5!1s0x882b34d68bf33a9b:0x15edd8c4de1c7581!8m2!3d43.6425662!4d-79.3870568!16zL20vMDF0d3M?entry=ttu",
+        attraction:"CN Tower",
+        date:"24 July, 2024 - 30 July, 2024",
+        description:"The CN Tower is a 553.3 m-high concrete communications and observation tower in Toronto, Ontario, Canada. Completed in 1976, it is located in downtown Toronto, built on the former Railway Lands. Its name 'CN' referred to Canadian National, the railway company that built the tower."
+    },
+    {
+        id:2,
+        img:"https://upload.wikimedia.org/wikipedia/commons/b/bf/Golden_Gate_Bridge_as_seen_from_Battery_East.jpg",
+        name:"San Francisco",
+        googleMaps:"https://www.google.com/maps/place/Golden+Gate+Bridge/@37.8199109,-122.4811347,17z/data=!3m1!4b1!4m6!3m5!1s0x808586deffffffc3:0xcded139783705509!8m2!3d37.8199109!4d-122.4785598!16zL20vMDM1cDM?entry=ttu",
+        attraction:"Golden Gate Bridge",
+        date:"20 December, 2023 - 2 January, 2024",
+        description:"Acclaimed as one of the world's most beautiful bridges, there are many different elements to the Golden Gate Bridge that make it unique. With its tremendous towers, sweeping cables, and great span, the Bridge is a sensory beauty and engineering wonder featuring color, sound and light."
+    },
+    {
+        id:3,
+        img:"https://www.universalorlando.com/webdata/upr/en/us/files/images/universal-parks-globe-b-crop.jpg",
+        name:"Orlando",
+        googleMaps:"https://www.google.com/maps/place/Universal+Studios+Florida/@28.4793754,-81.4711171,17z/data=!3m1!4b1!4m6!3m5!1s0x88e77ec39415df75:0xf985d8fc7734a5a1!8m2!3d28.4793754!4d-81.4685422!16zL20vMDVxc3lx?entry=ttu",
+        attraction:"Universal Studios",
+        date:"20 October, 2023 - 25 October, 2023",
+        description:"Universal Studios Florida is a theme park located in Orlando, Florida, that opened on June 7, 1990. Owned and operated by NBCUniversal, it features numerous rides, attractions, and live shows that are primarily themed to movies, television, and other aspects of the entertainment"
+    }
+]
