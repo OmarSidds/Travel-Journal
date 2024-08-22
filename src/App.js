0f5258254c61f1f578f6c731f1b0b13e205a827a
OmarSidds <101479068+OmarSidds@users.noreply.github.com>
2024-08-22 16:39:40 -0400

    Added data and started styling code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import './App.css';
 import Navbar from './Navbar.js';
 import data from './data.js';
+import Main from './Main.js'
 
-function App() {
-  const 
+function App() { 
+  const cardData = data.map(item => {
+    return (
+        <Main 
+          key={item.id}
+          item={item}  
+        />
+    )
+  })
 
   return (
     <div className="App">
       <Navbar />
+      <div className='space'></div>
+      {cardData}
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import './Main.css';
 
-export default function Main() {
+export default function Main(props) {
     return (
-        
+        <div className='mainContent'>
+            <img src={props.item.img} className='locationPicture'/>
+            <div className='textContent'>
+                <div className='locationMap'>
+                    <span class="material-symbols-outlined">location_on</span>
+                    <h1>{props.item.name}</h1>
+                    <a href={props.item.googleMaps}>View on Google Maps</a>
+                </div>
+                <h2>{props.item.attraction}</h2>
+                <h3>{props.item.date}</h3>
+                <p>{props.item.description}</p>
+            </div>
+        </div>
     )
 }
